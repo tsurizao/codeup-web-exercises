750090e60f796247bba2c61eaa3fcc3fdf04d2c3
Chase Martinez <chasemartinez22@gmail.com>
2022-04-07 11:24:57 -0500

    Adjusted wording and small changes to make code more robust.

--- js/external.js
+++ js/external.js
@@ -9,28 +9,29 @@ alert("Really?! My favorite color is " + userFavoriteColor + " too!");
 
 // Problem 1
 alert("Now onto some questions...");
+alert("Problem #1:");
 alert("You rented some movies for your kids: The Little Mermaid, Brother Bear and Hercules.");
 var littleMermaidDaysRented = parseFloat(prompt("How many days did you rent 'The Little Mermaid'?"));
 var brotherBearDaysRented = parseFloat(prompt("How many days did you rent 'Brother Bear'?"));
 var herculesDaysRented = parseFloat(prompt("How many days did you rent 'Hercules'?"));
 var movieRentalFeePerDayDollars = parseFloat(prompt("How much is the daily rental cost per movie?"));
 var totalRentalCost = movieRentalFeePerDayDollars * (littleMermaidDaysRented + brotherBearDaysRented + herculesDaysRented);
-alert("Your total cost is $" + totalRentalCost);
+alert("Your total cost is $" + totalRentalCost.toFixed(2));
 
 // Problem 2
-alert("Great!  Now problem #2:");
+alert("Problem #2:");
 alert("Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour.");
-var googleHourlyPayRateDollars = prompt("How much does Google pay you per hour?");
-var googleHoursWorked = prompt("How many hours did you work for Google this week?");
-var amazonHourlyPayRateDollars = prompt("How much does Amazon pay you per hour?");
-var amazonHoursWorked = prompt("How many hours did you work for Amazon this week?");
-var facebookHourlyPayRateDollars = prompt("How much does Facebook pay you per hour?");
-var facebookHoursWorked = prompt("How many hours did you work for Facebook this week?");
-var totalPayForAllWork = ((googleHourlyPayRateDollars * googleHoursWorked) + (amazonHourlyPayRateDollars * amazonHoursWorked) + (facebookHourlyPayRateDollars * facebookHoursWorked));
-alert("You made $" + totalPayForAllWork + " this week!");
+var googleHourlyRateDollars = parseFloat(prompt("How much does Google pay you per hour?"));
+var amazonHourlyRateDollars = parseFloat(prompt("How much does Amazon pay you per hour?"));
+var facebookHourlyRateDollars = parseFloat(prompt("How much does Facebook pay you per hour?"));
+var googleHoursWorked = parseFloat(prompt("How many hours did you work for Google this week?"));
+var amazonHoursWorked = parseFloat(prompt("How many hours did you work for Amazon this week?"));
+var facebookHoursWorked = parseFloat(prompt("How many hours did you work for Facebook this week?"));
+var totalPayForAllWork = ((googleHourlyRateDollars * googleHoursWorked) + (amazonHourlyRateDollars * amazonHoursWorked) + (facebookHourlyRateDollars * facebookHoursWorked));
+alert("You made $" + totalPayForAllWork.toFixed(2) + " this week!");
 
 // Problem 3
-alert("Problem numero 3:");
+alert("Problem #3:");
 alert("You can enroll in a class only if the class is not full and the class schedule does not conflict with your schedule.");
 var isClassFull = confirm("Is the class full? (Cancel - No | Ok - Yes)");
 var isScheduleConflict = confirm("Does the class schedule conflict with your personal schedule? (Cancel - No | Ok - Yes)");
@@ -42,7 +43,7 @@ if (canYouEnroll){
 }
 
 // Problem 4
-alert("Final Problem");
+alert("Problem #4:");
 alert("A product offer can be applied only if a person buys more than 2 items, and the offer has not expired.  Premium members do not need to buy a specific amount of products.");
 var hasMoreThanTwoItems = confirm("Did you buy at least 3 items? (Cancel - No | Ok - Yes)");
 var isOfferValid = confirm("Is the offer still valid? (Cancel - No | Ok - Yes)");
