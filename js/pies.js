53b403c8508e36ab483d259349203a533b303c94
Chase Martinez <chasemartinez22@gmail.com>
2022-04-14 17:27:19 -0500

    Add extra mini exercises

---
+++ js/pies.js
@@ -0,0 +1,8 @@
+(function(){
+    var pies = ["apple", "cherry", "key lime", "huckleberry", "rhubarb"];
+
+    function recentlyBakedPies(array){
+        return pies.slice(array.length - 3);
+    }
+
+}());
