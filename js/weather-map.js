8fcc58558bcfd83f325b75f8f284712607fe596f
Chase Martinez <chasemartinez22@gmail.com>
2022-05-20 10:26:53 -0500

    Changed the ajax request to GET

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,10 +1,10 @@
 "use strict";
 
 // (function () {
-    $.ajax("http://api.openweathermap.org/data/2.5/weather", {
-        APPID: OPEN_WEATHER_KEY,
-        q: "San Antonio, US"
-    }).done(function (data) {
-        console.log(data);
-    });
+$.get("http://api.openweathermap.org/data/2.5/weather", {
+    APPID: OPEN_WEATHER_KEY,
+    q: "San Antonio, US"
+}).done(function (data) {
+    console.log(data);
+});
 // }());
