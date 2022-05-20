3e4ba418943869c9f4b9f0a45fba31a30c76272a
Chase Martinez <chasemartinez22@gmail.com>
2022-05-20 10:24:23 -0500

    Commented out IIFE

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,8 +1,10 @@
 "use strict";
 
-(function () {
+// (function () {
     $.ajax("http://api.openweathermap.org/data/2.5/weather", {
         APPID: OPEN_WEATHER_KEY,
         q: "San Antonio, US"
+    }).done(function (data) {
+        console.log(data);
     });
-}());
+// }());
