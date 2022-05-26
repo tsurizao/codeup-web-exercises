c32a555de92ffffd06a607f420426c04ded54b4f
Chase Martinez <chasemartinez22@gmail.com>
2022-05-26 17:08:05 -0500

    Add promises.js file

---
+++ js/promises.js
@@ -0,0 +1,12 @@
+// (function(){
+
+"use strict";
+
+let getLatestCommit = (username) => {
+    fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"Authorization": "GITHUB_TOKEN"}})
+        .then((res) => res.json())
+        .then((res) => console.log(res[0].payload))
+}
+
+getLatestCommit("tsurizao");
+// }());
