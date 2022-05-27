705fc1ea46b98266449d14c442c2848c7e3bb8b3
Chase Martinez <chasemartinez22@gmail.com>
2022-05-27 12:16:13 -0500

    Updating exercise file

--- js/promises.js
+++ js/promises.js
@@ -2,10 +2,10 @@
 
 "use strict";
 
-let getLatestCommit = (username) => {
-    fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"Authorization": "GITHUB_TOKEN"}})
+let getLatestCommit = username => {
+    fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"Authorization": GITHUB_TOKEN}})
         .then((res) => res.json())
-        .then((res) => console.log(res[0].payload))
+        .then((res) => console.log(res[0].created_at));
 }
 
 getLatestCommit("tsurizao");
