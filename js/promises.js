7a706ee974ab422d3e0da447bcb9c3217aec88c9
Chase Martinez <chasemartinez22@gmail.com>
2022-05-27 13:30:16 -0500

    Feat: completed promises.js exercises

--- js/promises.js
+++ js/promises.js
@@ -2,11 +2,31 @@
 
 "use strict";
 
-let getLatestCommit = username => {
+// Function accepts a username as a parameter and returns the latest Github commit
+const getLatestCommit = username => {
     fetch(`https://api.github.com/users/${username}/events/public`, {headers: {"Authorization": GITHUB_TOKEN}})
-        .then((res) => res.json())
-        .then((res) => console.log(res[0].created_at));
+        .then((response) => response.json())
+        .then((response) => console.log(response[0].created_at));
+}
+getLatestCommit("tsurizao");
+
+// Function that accepts number of seconds, the timeout converts seconds to milliseconds
+// before running, then resolve returns seconds
+const wait = seconds => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(seconds);
+        }, seconds * 1000);
+    });
 }
 
-getLatestCommit("tsurizao");
+// Function call with an argument of 3 passed to the seconds parameter
+wait(3).then((response) => {
+    if (response === 1) {
+        console.log(`${response} second has passed.`)
+    } else {
+        console.log(`${response} seconds have passed.`)
+    }
+});
+
 // }());
