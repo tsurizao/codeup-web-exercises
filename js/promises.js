c7327e184236458336138bb03ae58d60361cf1d0
Chase Martinez <chasemartinez22@gmail.com>
2022-05-31 10:55:13 -0500

    fix import/export code now works with all files associated tokens

--- js/promises.js
+++ js/promises.js
@@ -1,31 +1,34 @@
-(function(){
+// (function () {
+    "use strict";
 
-"use strict";
-
-const baseURL = "https://api.github.com/users/"
+    import {GITHUB_TOKEN} from "./keys.js";
+    const baseURL = "https://api.github.com/users/"
 
 // Function accepts a username as a parameter and returns the latest Github commit
-const getLatestCommit = username => {
-    fetch(`${baseURL}${username}/events/public`, {headers: {"Authorization": "token " + GITHUB_TOKEN}})
-        .then((response) => response.json())
-        .then((response) => console.log(response[0].created_at));
-}
-getLatestCommit("tsurizao");
+    const getLatestCommit = username => {
+        fetch(`${baseURL}${username}/events/public`, {headers: {"Authorization": "token " + GITHUB_TOKEN}})
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response[0].created_at);
+                return response[0].created_at
+            });
+    }
+    getLatestCommit("tsurizao");
 
 // Function that accepts number of seconds, the timeout converts seconds to milliseconds
 // before running, then resolve returns seconds
-const wait = seconds => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(seconds);
-        }, seconds);
-    });
-}
+    const wait = seconds => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(seconds);
+            }, seconds);
+        });
+    }
 
 // Function call with an argument of 3 passed to the seconds parameter
-wait(3000).then((response) => {
-    const seconds = response / 1000;
-    seconds === 1 ? console.log(`${seconds} second has passed.`) : console.log(`${seconds} seconds have passed.`);
-});
+    wait(3000).then((response) => {
+        const seconds = response / 1000;
+        seconds === 1 ? console.log(`${seconds} second has passed.`) : console.log(`${seconds} seconds have passed.`);
+    });
 
-}());
+// }());
